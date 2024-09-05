d98668803a908245e6f42f74f9058aedb6f787e6
nahid-dev <nahidv232@gmail.com>
2024-09-06 00:58:12 +0600

    middlware completed

--- src/utils/Cloudinary.js
+++ src/utils/Cloudinary.js
@@ -7,3 +7,21 @@ cloudinary.config({
     api_key: process.env.CLOUDINRAY_API_KEY,
     api_secret: process.env.CLOUDINRAY_API_SECRET,
 });
+
+const uploadCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+        // upload the file on cloudinary
+        const response = cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto",
+        });
+        //file has been uploaded successfully
+        console.log("File is uploaded cloudinary.", response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath); //remove the locally save temporary file as the upload operation got failed.
+        return null;
+    }
+};
+
+export { uploadCloudinary };
