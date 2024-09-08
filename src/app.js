06c6aa3a47cffe550618ae29d2bc80a80eb95f7b
nahid-dev <nahidv232@gmail.com>
2024-09-09 01:02:23 +0600

    router creating completed

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-const app = require(express);
+// const app = require(express);
+const app = express()
 app.use(
     cors({
         origin: process.env.CORS_ORIGIN,
@@ -14,5 +15,11 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+
+//Routes import
+import userRouter from "./routes/user.routes.js"
+
+app.use("/api/v1/users", userRouter);
 
 export { app };
